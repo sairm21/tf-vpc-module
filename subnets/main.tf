a146ec1fb01b3f3380fc733358c17ab4748ca476
sairam <sairm21@gmail.com>
2023-09-29 22:17:48 +0530

    subnet creation in vpc local submodule

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,6 +1,5 @@
 resource "aws_subnet" "main" {
   vpc_id     = var.vpc_id
-  cidr_block = "10.0.1.0/24"
   count = length(var.cidr_block)
   cidr_block = element(var.cidr_block, count.index)
   availability_zone = element(var.az, count.index)
