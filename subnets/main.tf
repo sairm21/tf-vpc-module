6bdbb30164c707ce8f426c3061597d261bdf8fcd
sairam <sairm21@gmail.com>
2023-10-06 08:19:39 +0530

    adding route table to subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -10,3 +10,24 @@ resource "aws_subnet" "main" {
   },
     var.tags)
 }
+
+resource "aws_route_table" "example" {
+  vpc_id = var.vpc_id
+
+/*
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.example.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.example.id
+  }
+*/
+
+  tags = merge({
+    Name = "${var.subnet_name}-${var.env}-RT"
+  },
+    var.tags)
+}
