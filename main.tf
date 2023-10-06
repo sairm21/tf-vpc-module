fc228e89e05e9358e4d1c993e9901b6b3618023d
sairam <sairm21@gmail.com>
2023-10-06 21:43:03 +0530

    default vpc RT peering.

--- main.tf
+++ main.tf
@@ -72,3 +72,9 @@ resource "aws_route" "peer-route" {
   destination_cidr_block    = "172.31.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.VPC_peering.id
 }
+
+resource "aws_route" "default-vpc-peer-route" {
+  route_table_id            = var.default_Route_table_ID
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.VPC_peering.id
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,4 @@ variable "az" {
   default = ["us-east-1a", "us-east-1b"]
 }
 variable "default_VPC_id" {}
+variable "default_Route_table_ID" {}
